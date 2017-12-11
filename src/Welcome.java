d9d2a8f45f61008505727396715089790d5e3f73
549781 <549781@PC317557.cts.com>
2017-12-11 19:40:19 +0530

    this is a check in by chandhana

--- src/Welcome.java
+++ src/Welcome.java
@@ -4,6 +4,8 @@ public class Welcome {
 	public static void main(String[] args){
 		System.out.println("test is this pro");
 		System.out.println("first checkin completed try to do second");
+		System.out.println("second checkin completed try to do second");
+		
 	}
 
 }
