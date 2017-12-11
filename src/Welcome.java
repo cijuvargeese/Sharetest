17c54147abf7c0d642edf01070c3602b2fecf060
549750 <549750@PC315533.cts.com>
2017-12-11 19:18:40 +0530

    secon checkin

--- src/Welcome.java
+++ src/Welcome.java
@@ -3,6 +3,7 @@
 public class Welcome {
 	public static void main(String[] args){
 		System.out.println("test is this pro");
+		System.out.println("first checkin completed try to do second");
 	}
 
 }
